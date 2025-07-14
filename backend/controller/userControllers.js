695ecd6760fec41c777db137a518710455a31c53
amitpanwar789 <amitpanwar02705@gmail.com>
2025-07-14 08:30:21 +0530

    update user fixed

--- backend/controller/userControllers.js
+++ backend/controller/userControllers.js
@@ -149,8 +149,9 @@ const updateUser = AsyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    user.isAdmin = req.body.isAdmin || user.isAdmin;
-
+    if(req.body.isAdmin != null) user.isAdmin = req.body.isAdmin 
+    
+    
     const updatedUser = await user.save();
 
     res.json({
